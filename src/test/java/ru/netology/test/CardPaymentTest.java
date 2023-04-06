0521e5a733a8efbcecb0ea5553887c3a7537c3a3
Roman Mukovnin <r.mykovnin@gmail.com>
2023-04-07 00:00:14 +0300

    fixed build

--- src/test/java/ru/netology/test/CardPaymentTest.java
+++ src/test/java/ru/netology/test/CardPaymentTest.java
@@ -5,31 +5,48 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
 import ru.netology.data.SQLHelper;
+import ru.netology.page.StartPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardPaymentTest {
 
     @BeforeAll
     static void setUpAll() {
+
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterAll
     static void tearDownAll() {
+
         SelenideLogger.removeListener("allure");
     }
 
     @BeforeEach
     void setup() {
+
         open("http://localhost:8080");
     }
 
     @BeforeEach
     void clearDataBase() {
+
         SQLHelper.cleanDataBase();
     }
 
+    @Test
+    public void shouldValidValues() {
+        var startPage = new StartPage();
+        var cardPayment = startPage.cardPayment();
+        cardPayment.fillingPaymentForm(DataHelper.getCardInfoValidValues());
+        cardPayment.successfulPayment();
+        var paymentStatus = SQLHelper.getStatusCardPayment();
+        assertEquals("APPROVED", paymentStatus);
+    }
 
 }
